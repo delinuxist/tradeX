d1f8ba3d0a0ea90a360c1e9d518a503f5bd5166b
Emmanuel Obeng Twene <emmanuel.twene@turntabl.io>
2023-06-10 01:24:43 +0000

    constants to help map the stocks to its respective image

---
+++ src/app/core/constants/stockImg.ts
@@ -0,0 +1,74 @@
+export const tickerMap = {
+  AAPL: {
+    name: 'Apple Inc',
+    image:
+      'https://loodibee.com/wp-content/uploads/Apple-Logo-1998-present.png',
+  },
+  MSFT: {
+    name: 'Microsoft',
+    image: '../../../assets/img/microsoft.png',
+  },
+  IBM: {
+    name: 'IBM',
+    image: 'https://companieslogo.com/img/orig/IBM-57502b16.png?t=1669436923',
+  },
+  NFLX: {
+    name: 'Netflix',
+    image:
+      'https://cdn.iconscout.com/icon/free/png-512/free-netflix-3628944-3030169.png?f=avif&w=256',
+  },
+  GOOGL: {
+    name: 'Google',
+    image:
+      'https://cdn.iconscout.com/icon/free/png-256/free-google-2981831-2476479.png?f=webp',
+  },
+  TSLA: {
+    name: 'Tesla',
+    image:
+      'https://cdn.iconscout.com/icon/free/png-256/free-tesla-14-892143.png',
+  },
+  ORCL: {
+    name: 'Oracle',
+    image:
+      'https://companieslogo.com/img/orig/ORCL_BIG-18499385.png?t=1633210264',
+  },
+  AMZN: {
+    name: 'Amazon',
+    image: '../../../assets/img/amazon.png',
+  },
+};
+
+// export const tickerMap = {
+//   AAPL: {
+//     name: 'Apple Inc',
+//     image: '../../../assets/img/apple.png',
+//   },
+//   MSFT: {
+//     name: 'Microsoft',
+//     image: '../../../assets/img/msft.png',
+//   },
+//   IBM: {
+//     name: 'IBM',
+//     image: '../../../assets/img/ibm.png',
+//   },
+//   NFLX: {
+//     name: 'Netflix',
+//     image: '../../../assets/img/netflix.png',
+//   },
+//   GOOGL: {
+//     name: 'Google',
+//     image: '../../../assets/img/google.png',
+//   },
+//   TSLA: {
+//     name: 'Tesla',
+//     image: '../../../assets/img/tesla.webp',
+//   },
+//   ORCL: {
+//     name: 'Oracle',
+//     image: '../../../assets/img/oracle.jpg',
+//   },
+//   AMZN: {
+//     name: 'Amazon',
+//     image: '../../../assets/img/amazon.png',
+//   },
+// };
